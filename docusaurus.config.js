8235eb7927b314390f5160b02c284134c284d460
LordCat <noahcatz@outlook.com>
2026-05-10 21:40:16 -0400

    Add basic packages and config

---
+++ docusaurus.config.js
@@ -0,0 +1,60 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'NitroBolt Documentation',
+  url: 'https://docs.nitrobolt.org',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'NitroBolt',
+  projectName: 'docs',
+  trailingSlash: false,
+  themeConfig: {
+    navbar: {
+      title: 'NitroBolt Documentation',
+      items: [
+        {
+          href: '/packager/',
+          label: 'Packager',
+          position: 'left'
+        },
+        {
+          href: '/development/',
+          label: 'Development',
+          position: 'left'
+        },
+        {
+          href: 'https://nitrobolt.org/',
+          label: 'NitroBolt',
+          position: 'right'
+        },
+        {
+          href: 'https://github.com/NitroBolt',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    prism: {
+      theme: require('./code-themes/light'),
+      darkTheme: require('./code-themes/dark'),
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+          breadcrumbs: false,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
