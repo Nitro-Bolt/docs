d34582de81b68a4fcf355bcad12635334ad45e33
ddededodediamante <lukitadiamante5@gmail.com>
2026-08-10 20:25:31 -0300

    branches

--- docs/extensions/branches.md
+++ docs/extensions/branches.md
@@ -10,7 +10,7 @@ Branches allow you to easily create C-shaped blocks like conditionals and loops.
 To use branches in your extension, you can use the `branchCount` property.
 
 :::note
-`branchCount` can only be used in blocks with their block type set to `LOOP`, `CONDITIONAL` or `REPORTER`.
+`branchCount` can only be used in blocks with their block type set to `LOOP` or `CONDITIONAL`.
 `LOOP` is recommended for blocks meant to be loops, as an icon will be added to indicate that it's mean to be a loop.
 :::
 
@@ -18,3 +18,4 @@ Here is an example:
 
 <ExtensionCode title="branches">{require('!raw-loader!@site/static/example-extensions/branches.js')}</ExtensionCode>
 
+Putting code inside of this block will run it normally.
--- static/example-extensions/branches.js
+++ static/example-extensions/branches.js
@@ -9,12 +9,6 @@ class Branches {
           blockType: Scratch.BlockType.CONDITIONAL,
           text: ["hello", "neighbor"],
           branchCount: 1
-        },
-        {
-          opcode: "codeStatus",
-          blockType: Scratch.BlockType.REPORTER,
-          text: ["run code", "and return status"],
-          branchCount: 1
         }
       ]
     };
@@ -26,16 +20,6 @@ class Branches {
       1, /* the number of the branch */
       false /* is it going to loop? */
     );
-  }
-
-  codeStatus(args, util) {
-    try {
-      util.startBranch(1, false);
-      console.log("uuhhh");
-      return "success";
-    } catch (error) {
-      return "error: " + error;
-    }
   }
 }
 Scratch.extensions.register(new Branches());
